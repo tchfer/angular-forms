bebd7d87762103b66c51e6b14073d17f9d56b940
tchfer <tchfernando@gmail.com>
2023-07-20 03:14:33 -0300

    Add form completion on zip code insertion

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,36 +1,47 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ConsultaCepService } from '../service/consulta-cep.service';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../service/consulta-cep.service";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
-
   constructor(
     private router: Router,
     private consultaCepService: ConsultaCepService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  consultaCEP(event: any) {
+  consultaCEP(event: any, form: NgForm) {
     const cep = event?.target?.value;
-    return this.consultaCepService.getConsultaCep(cep).subscribe(
-      response => console.log(response));
+    if (cep !== '') {
+      this.consultaCepService.getConsultaCep(cep).subscribe((response) => {
+        console.log(response);
+        this.popularEndereco(response, form)
+      });
+    }
   }
 
-  cadastrar(form: NgForm){
-    if(form.valid) {
-      this.router.navigate(['./sucesso'])
+  popularEndereco(data: any, form: NgForm) {
+    form.form.patchValue({
+      endereco: data.logradouro,
+      complemento: data.complemento,
+      bairro: data.bairro,
+      cidade: data.localidade,
+      estado: data.uf,
+    })
+  }
+
+  cadastrar(form: NgForm) {
+    if (form.valid) {
+      this.router.navigate(["./sucesso"]);
     } else {
-      alert('Preencha os campos corretamente!');
+      alert("Preencha os campos corretamente!");
     }
     console.log(form.controls);
-    
   }
 }
